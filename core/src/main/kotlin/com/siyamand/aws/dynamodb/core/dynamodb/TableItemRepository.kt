eff107c09b82adae2079108018d16526fa06d75f
siyamand <siyamand@orchardcollaboration.com>
2021-02-04 11:01:19 +0100

    Extend TableItemRepository to fetch item by Key

--- core/src/main/kotlin/com/siyamand/aws/dynamodb/core/dynamodb/TableItemRepository.kt
+++ core/src/main/kotlin/com/siyamand/aws/dynamodb/core/dynamodb/TableItemRepository.kt
@@ -8,4 +8,5 @@ import kotlinx.coroutines.flow.Flow
 interface TableItemRepository : AWSBaseRepository {
     suspend fun add(tableItem: TableItemEntity): TableItemEntity
     fun getList(tableName:String, startKey: Map<String, AttributeValueEntity>?): Flow<TableItemEntity>
+    suspend fun getItem(tableName: String, key: Map<String, AttributeValueEntity>): List<TableItemEntity>
 }
--- infrastructure/src/main/kotlin/com/siyamand/aws/dynamodb/infrastructure/repositories/DynamodbTableItemRepositoryImpl.kt
+++ infrastructure/src/main/kotlin/com/siyamand/aws/dynamodb/infrastructure/repositories/DynamodbTableItemRepositoryImpl.kt
@@ -9,8 +9,10 @@ import com.siyamand.aws.dynamodb.infrastructure.mappers.TableItemtMapper
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.reactive.asFlow
 import kotlinx.coroutines.reactive.awaitFirst
+import reactor.core.publisher.Mono
 import reactor.core.publisher.Mono.fromFuture
 import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue
 import software.amazon.awssdk.services.dynamodb.model.QueryRequest
 
 class DynamodbTableItemRepositoryImpl(private val clientBuilder: ClientBuilder) : TableItemRepository, AwsBaseRepositoryImpl() {
@@ -22,20 +24,50 @@ class DynamodbTableItemRepositoryImpl(private val clientBuilder: ClientBuilder) 
         return fromFuture(returnValue).awaitFirst()
     }
 
-    override fun getList(tableName:String, startKey: Map<String, AttributeValueEntity>?): Flow<TableItemEntity> {
+    override fun getList(tableName: String, startKey: Map<String, AttributeValueEntity>?): Flow<TableItemEntity> {
         val db = getClient(clientBuilder::buildAsyncDynamodb)
         val requestBuilder = QueryRequest.builder().tableName(tableName)
-        if(startKey != null){
+        if (startKey != null) {
             requestBuilder.exclusiveStartKey(TableItemtMapper.convertKey(startKey))
         }
         val response = db.queryPaginator(requestBuilder.build())
         val returnValue = response.items().map { it ->
             val tableItemEntity = TableItemEntity(tableName)
-            tableItemEntity.attributes.putAll(it.mapValues {entry ->  TableItemtMapper.convertToAttributeValueEntity(entry.value) })
+            tableItemEntity.attributes.putAll(it.mapValues { entry -> TableItemtMapper.convertToAttributeValueEntity(entry.value) })
             tableItemEntity
         }
 
-        return  returnValue.asFlow()
+        return returnValue.asFlow()
+    }
+
+    override suspend fun getItem(tableName: String, key: Map<String, AttributeValueEntity>): List<TableItemEntity> {
+        val db = getClient(clientBuilder::buildAsyncDynamodb)
+        val query = convert(tableName, key)
+
+        val response = db.query(query).thenApply {
+            it.items().map { it ->
+                val tableItemEntity = TableItemEntity(tableName)
+                tableItemEntity.attributes.putAll(it.mapValues { entry -> TableItemtMapper.convertToAttributeValueEntity(entry.value) })
+                tableItemEntity
+            }
+        }
+
+        return fromFuture(response).awaitFirst()
+    }
+
+    private fun convert(tableName: String, key: Map<String, AttributeValueEntity>) :QueryRequest {
+        val requestBuilder = QueryRequest.builder().tableName(tableName)
+        val values = mutableMapOf<String, AttributeValue>()
+        val expression = key.map {
+            val expr = "${it.key} = :${it.key}"
+            values.putIfAbsent(":${it.key}", TableItemtMapper.convertAttributeValue(it.value))
+            expr
+        }.joinToString(separator = " and ")
+
+        requestBuilder.keyConditionExpression(expression)
+        requestBuilder.expressionAttributeValues(values)
+
+        return requestBuilder.build()
     }
 
     private fun asyncDynamoDb(): DynamoDbAsyncClient {
