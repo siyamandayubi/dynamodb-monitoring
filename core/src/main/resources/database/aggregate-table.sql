3cc9412ceb02fe91df3c8e66b22af1b9236943da
siyamand <siyamand@orchardcollaboration.com>
2021-02-24 17:35:29 +0100

    change the table creation sql to "Create IF NOT EXISTS"

--- core/src/main/resources/database/aggregate-table.sql
+++ core/src/main/resources/database/aggregate-table.sql
@@ -1,11 +1,13 @@
-CREATE TABLE `${tableName}` (
+CREATE TABLE IF NOT EXISTS `${tableName}` (
+  `Hash` VARCHAR(64) NOT NULL,
+  `StartDate` DATE,
+  `EndDate` DATE,
   `GroupValue` NVARCHAR(100) NOT NULL,
   `FieldName` NVARCHAR(100) NOT NULL,
   `FieldValue` NVARCHAR(100) NULL,
-  `StartDate` DATETIME NULL,
-  `EndDate` DATETIME NULL,
   `Count` INT NULL,
   `Max` NVARCHAR(100) NULL,
   `MIN` NVARCHAR(100) NULL,
-  `Hash` VARCHAR(64) NOT NULL,
-  PRIMARY KEY (`Hash`));
+  `SUM` DOUBLE NULL,
+
+  PRIMARY KEY (`Hash`,`StartDate`,`EndDate`));
