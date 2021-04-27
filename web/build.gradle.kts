abfb65027660daec08d3948faa448ab58ff90da2
siyamand <siyamand@orchardcollaboration.com>
2021-04-27 16:48:42 +0200

    add jjwt implementation to the package

--- web/build.gradle.kts
+++ web/build.gradle.kts
@@ -23,8 +23,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.4.2")
     implementation("io.jsonwebtoken:jjwt-api:0.11.2")
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.4.21")
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
-    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
+    implementation("io.jsonwebtoken:jjwt-impl:0.11.2")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.11.2")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
